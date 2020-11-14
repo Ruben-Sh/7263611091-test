8d061820bfd2ca5cf366c99f98d18ede0b7a97a6
Ruben-Sh <72896766+Ruben-Sh@users.noreply.github.com>
2020-11-14 15:41:01 +1100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,12 @@ export const get = async (transaction) => {
   //init authorization
   return await (new ECS.Core(transaction)).run(async (core) => {
     //Create some users
-    for (let index = 0; index < 1; index++) {
-      const addResp = await core.db.user.addUser({
-        firstName: 'John', lastName: 'Doe', email: 'john@doe.com'
-    });  
-      if (!addResp) return IKAPI.IKBadRequestResponse('Could not create user!', ECS.Globals.ErrorCode_IncorrectBehaviour);  
-    }
+    //for (let index = 0; index < 1; index++) {
+    //  const addResp = await core.db.user.addUser({
+    //    firstName: 'John', lastName: 'Doe', email: 'john@doe.com'
+    //});  
+    //  if (!addResp) return IKAPI.IKBadRequestResponse('Could not create user!', ECS.Globals.ErrorCode_IncorrectBehaviour);  
+    //}
     //Fetch all user
     const resp = await core.db.user.findAllUsers();
     if (!resp) return IKAPI.IKBadRequestResponse('Users not found', ECS.Globals.ErrorCode_IncorrectBehaviour);
